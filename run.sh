5ca6dd5dd9964dae9efcf92964d8edeb401f1143
庞亚菲 <root@igis-dev1.whrdc>
2025-07-04 09:11:54 +0800

    neo4j 配置更新

--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@ docker run -d \
     --publish=7474:7474 --publish=7687:7687 \
     --volume=$PWD/volumes/neo4j/data:/data \
     --name neo4j \
+    -e NEO4J_PLUGINS='["apoc"]' \
+    -e NEO4J_dbms_security_procedures_unrestricted=apoc.* \
+    -e NEO4J_dbms_security_procedures_whitelist=apoc.* \
     neo4j:4.4.26
